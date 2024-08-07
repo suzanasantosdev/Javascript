116657f0d5cf37e33ea4b5e57f39a41f937d4191
Suzana Santos <suzanajb1@gmail.com>
2024-08-07 16:03:41 -0300

    exercicio aula 03

--- exercicios/ex03/ex.js
+++ exercicios/ex03/ex.js
@@ -1,1 +1,3 @@
 console.log('Meu nome é "Suzana". Estou aprendendo JAVASCRIPT ás', 15, 'da tarde.');
+
+// para o numero nao ficar marcado como texto, tiramos ele da aspas e deixamos sozinho com uma virgula. assim o script ira ler ele como um numero e nao como um texto
