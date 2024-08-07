4a2b743ea02ad59a67c9ca1fafdbdcbe54f78e34
Suzana Santos <suzanajb1@gmail.com>
2024-08-07 15:07:56 -0300

    aprendendo sobre console.log

---
+++ exercicios/ex03/index.js
@@ -0,0 +1,8 @@
+//FUNCAO CONSOLE.LOG
+
+console.log ('Suzana Santos'); // STRING = TEXTO
+console.log("Suzana 'Santos'"); 
+console.log(`Suzana "Santos"`)
+// sempre que quiser exibir um texto ou um numero deve ser usado console.log()
+
+console.log(27, 18.24) // em javascript nao tem separacao de numeros tudo sao "NUMBER = NUMERO"
