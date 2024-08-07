0b34da2a570f3911921dd65ccc9f9c0aaa7e64a4
Suzana Santos <suzanajb1@gmail.com>
2024-08-07 16:27:13 -0300

    aprendendo sobre comentarios em js

---
+++ exercicios/ex04/comentarios.js
@@ -0,0 +1,14 @@
+// Aprendendo sobre comentarios ,quando colocamos comentarios no codigo ele nao ira ser impresso para o usuario apenas para o desenvolvedor.
+//Exemplo:
+
+console.log('Hello World!')
+console.log('Linha 1');
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+
+//vejamos que apenas o codigo foi impresso e todo o comentario foi ignorado.
+
+/* Esse tipo de comentario podemos quebrar 
+a linha sem que o comentario se desfaça*/
