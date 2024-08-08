6394653794447f3f7411656df964c1ccbb262f0b
Suzana Santos <suzanajb1@gmail.com>
2024-08-08 17:41:42 -0300

    exercício sobre constante

---
+++ exercicios/ex07/index.js
@@ -0,0 +1,9 @@
+// Aprendendo sobre Constantes
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log (resultadoTriplicado);
