c42b9938f561af4231ec938c73fb11a892f40677
Suzana Santos <suzanajb1@gmail.com>
2024-08-08 16:24:10 -0300

    exercício 06

---
+++ exercicios/ex06/index.js
@@ -0,0 +1,12 @@
+let nome = 'Joao';
+
+console.log (nome,'Nesceu em 1999');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.',);
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+//Nao podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Nao pode começar o nome de uma variáel com um número
+// Nao pode conter espaços ou traços
