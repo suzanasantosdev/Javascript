96cd38238ed888961c2273cc32be340b74669cee
Suzana Santos <suzanajb1@gmail.com>
2024-08-09 16:06:50 -0300

    tipos de dados primitivos.

---
+++ exercicios/ex010/index.js
@@ -0,0 +1,12 @@
+// DADOS PRIMITIVOS: NUMBER, UNDEFINED, NULL, BOOLEAN.
+
+const nome = 'Suzana'; // string
+const nome1 = "Suzana"; // string
+const nome2 = `Suzana`; // string
+const num1 = 10; //Number
+const num2 = 10.52; // Number
+let nomeAluno; // undefined = nao aponta pra local nenhuma na memoria
+const sobrenomeAluno = null; // Nulo -> nao aponta pra local nenhuma na memoria
+const aprovado = false; // Boolean = true, false (logico)
+
+console.log (typeof num1,num1);
