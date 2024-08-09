ae2c13ac6843ef73ac09136105a00ecd57675ba3
Suzana Santos <suzanajb1@gmail.com>
2024-08-09 18:24:21 -0300

    exercício operadores aritméticos

--- exercicios/ex011/index.js
+++ exercicios/ex011/index.js
@@ -1,4 +1,14 @@
 // Operadores aritmeticos, de atribuicao e incremento.
+/*
+ * = multiplicaçao
++ = soma
+- = subtraçao
+/ = divisao
+% = resto de divisao
+** = exponenciaçao
+++ = incrementaçao
+-- = decrementaçao
+ */
 
 const num1 = 10;
 const num2 = 3;
