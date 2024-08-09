7c408496035d38512a9e02edca20cd006660af38
Suzana Santos <suzanajb1@gmail.com>
2024-08-09 19:19:14 -0300

    colocando exercício em prática

---
+++ exercicios/ex011/exercicio.js
@@ -0,0 +1,7 @@
+const numero1 = 7;  // Simula a entrada do usuário
+const numero2 = 8;
+const numero3 = 9;
+
+const media = (numero1 + numero2 + numero3) / 3;
+
+console.log("A média dos três números é:", media);
