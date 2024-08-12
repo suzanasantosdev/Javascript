483c83697705783c432d85dae87020795c5aa206
Suzana Santos <suzanajb1@gmail.com>
2024-08-12 20:32:41 -0300

    ex014

---
+++ exercicios/ex014/index.js
@@ -0,0 +1,9 @@
+/*  uma string é uma sequência de caracteres utilizada para representar texto. Strings são usadas para armazenar e manipular palavras, frases, ou qualquer outro tipo de dados textuais. Elas são definidas usando aspas simples ('...'), aspas duplas ("..."), ou crases para strings de template (`...`).*/
+// EXEMPLOS
+
+let umaString = "Um\"texto\"";
+
+console.log(umaString.concat( 'Em um lindo dia.'));
+console.log(umaString + 'Em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`);// essa e a mais usada.
+
