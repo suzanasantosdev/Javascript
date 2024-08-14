f731c6ff4e6d2a1c38aa5d3dbd116830381184bd
Suzana Santos <suzanajb1@gmail.com>
2024-08-14 17:18:01 -0300

    atualizei

--- exercicios/ex016/index.js
+++ exercicios/ex016/index.js
@@ -1,9 +1,9 @@
 let num1 = 9.54578;
-//let num2 = Math.floor(num1);
-//let num2 = Math.ceil(num1);
-//let num2 = Math.round(num1);
+//let num2 = Math.floor(num1);  Arredonda um número para baixo, para o inteiro mais próximo.
+//let num2 = Math.ceil(num1); Arredonda um número para cima, para o inteiro mais próximo
+//let num2 = Math.round(num1); Arredonda um número para o inteiro mais próximo.
 //console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
 //console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
-//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); Gera um número aleatório entre 0 (inclusivo) e 1 (exclusivo).
 
 console.log(aleatorio);
