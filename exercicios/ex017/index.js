5eb2c537d760e8da9f683854bac3b99cfdacddb5
Suzana Santos <suzanajb1@gmail.com>
2024-08-19 15:25:10 -0300

    aprendendo array

---
+++ exercicios/ex017/index.js
@@ -0,0 +1,42 @@
+/* Em JavaScript, um array é uma estrutura de dados usada para armazenar uma coleção de valores. Esses valores podem ser de qualquer tipo, como números, strings, objetos, ou até outros arrays. Arrays são úteis quando você precisa lidar com múltiplos itens de dados relacionados. */
+
+//Indexados: Os elementos em um array são organizados por índices, que começam em 0. 
+
+//               0       1        2          3      4
+let alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo 
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -2));
+
+// console.log(alunos[50]);
+
+// delete alunos[1];
+// console.log(alunos[1]);
+
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Fábio'); // Adiciona no começo
+// alunos.unshift('Luiza');
+
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza';  // Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos[3] = 'Luiza'; // Adicionando
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
