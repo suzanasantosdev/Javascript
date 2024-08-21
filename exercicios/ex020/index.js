301a811797af847067c82417d311bb99173f37a2
Suzana Santos <suzanajb1@gmail.com>
2024-08-21 15:56:22 -0300

    exercício valores primitivos

--- exercicios/ex020/index.js
+++ exercicios/ex020/index.js
@@ -1,4 +1,15 @@
 /*
 Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+
+Referencia (mutavel) - array, object, function
 */
+const a = {
+    nome: 'Suzana',
+    sobrenome: 'Santos'
+};
+const b = {...a};
+
+a.nome = 'Joao';
+console.log(a);
+console.log(b);
 
