834e30feaf09814f504b45f35e51473e8c5d8577
Suzana Santos <suzanajb1@gmail.com>
2024-08-25 17:30:26 -0300

    ex024

--- exercicios/ex024/index.js
+++ exercicios/ex024/index.js
@@ -2,3 +2,11 @@
 && -> false && true -> false "o valor mesmo"
 || -> true && false -> vai retornar "o valor verdadeiro"
 */
+
+function falaoi () {
+    return 'Oi';
+}
+
+const vaiExecutar = false;
+
+console.log(vaiExecutar && falaoi());
