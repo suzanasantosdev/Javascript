29f2f8c2533adb412bddbc369d01e58ab28f1647
Suzana Santos <suzanajb1@gmail.com>
2024-08-27 17:53:05 -0300

    condicoes de if e else

--- exercicios/ex025/index.js
+++ exercicios/ex025/index.js
@@ -1,5 +1,20 @@
-const hora = 10;
+/*
+Entre 0 - 11 - Bom dia
+Entre 12 - 17 - Boa tarde
+Entre 18 - 23 - Boa noite
+*/
 
-if (hora < 12) {
+// if pode ser usado sozinho
+// sempre que utilizar a palavra else, preciso de um if antes
+// Eu posso ter vários else if na checagem
+// só posso ter um else na checagem
+// Podemos usar condicoes sem else if, utilizando apenas if e else
+const hora = 18;
+
+if (hora >= 0 && hora <= 11) {
     console.log('Bom dia!');
+}   else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde!');
+}   else if (hora >= 18 && hora <= 23) {
+    console.log('Boa noite!')
 }
