4847fe767b5dc6abeb563a21b9ec3938bb283907
Suzana Santos <suzanajb1@gmail.com>
2024-09-01 19:20:50 -0300

    atualizei o exercicio 026

--- exercicios/ex026/index.js
+++ exercicios/ex026/index.js
@@ -1,5 +1,7 @@
 const numero = 10;
 
-if (numero >= 0) {
-    console.log('Sim, o número é maior igual a zero.')
+if (numero >= 0 && numero <= 5) {
+    console.log('Sim, o número é maior igual a zero.');
+} else {
+    console.log('O numero NAO esta entre 0 e 5.');
 }
