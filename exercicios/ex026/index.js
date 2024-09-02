523e03ce91c0b39f92d9ba35c8a235c7306cdf05
Suzana Santos <suzanajb1@gmail.com>
2024-09-02 16:19:14 -0300

    finalizei exercicio 026

--- exercicios/ex026/index.js
+++ exercicios/ex026/index.js
@@ -4,7 +4,11 @@
 const numero = 10;
 
 if (numero >= 0 && numero <= 5) {
-    console.log('Sim, o número é maior igual a zero.');
+    console.log('O numero esta entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O numero esta entre 6 e 8');
+} else if (numero >= 9 && numero <= 11) { //verdadeiro
+    console.log('O numero esta entre 9 e 11.');
 } else {
-    console.log('O numero NAO esta entre 0 e 5.');
+    console.log('O numero nao esta entre 0 e 11.');
 }
