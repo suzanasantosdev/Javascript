1e8eea7671ceba7af9d73158277463b6ed198731
Suzana Santos <suzanajb1@gmail.com>
2024-09-12 17:12:46 -0300

    criei exercício 029

---
+++ exercicios/ex029/index.js
@@ -0,0 +1,22 @@
+const data = new Date('1987-04-21 00:00:00');
+const diaSemana = data.getDay();
+let diaSemanaTexto;
+
+if (diaSemana === 0) {
+    diaSemanaTexto = 'Domingo';
+} else if (diaSemana === 1) {
+    diaSemanaTexto = 'Segunda';
+} else if (diaSemana === 2) {
+    diaSemanaTexto = 'Terça';
+} else if (diaSemana === 3) {
+    diaSemanaTexto = 'Quarta';
+} else if (diaSemana === 4) {
+    diaSemanaTexto = 'Quinta';
+} else if (diaSemana === 5) {
+    diaSemanaTexto = 'Sexta';
+} else if (diaSemana === 6) {
+    diaSemanaTexto = 'Sabado';
+}
+
+
+console.log(diaSemana, diaSemanaTexto);
