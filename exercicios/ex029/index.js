d0c082e1fbcd98657e06ed3d1c5bc30014a7b4b7
Suzana Santos <suzanajb1@gmail.com>
2024-09-15 21:21:07 -0300

    finalizei o  ex029

--- exercicios/ex029/index.js
+++ exercicios/ex029/index.js
@@ -1,30 +1,38 @@
-const data = new Date('1987-04-25 00:00:00');
-const diaSemana = data.getDay();
-let diaSemanaTexto;
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
 
-switch (diaSemana) {
+    switch (diaSemana) {
     case 0:
-        diaSemanaTexto = 'Domingo';
-        break;
+    diaSemanaTexto = 'Domingo';
+    return diaSemanaTexto;
     case 1:
-        diaSemanaTexto = 'Segunda';
-        break;
+    diaSemanaTexto = 'Segunda';
+    return diaSemanaTexto;
     case 2:
-        diaSemanaTexto = 'Terca';
-        break;
+    diaSemanaTexto = 'Terça';
+    return diaSemanaTexto;
     case 3:
-        diaSemanaTexto = 'Quarta';
-        break;
+    diaSemanaTexto = 'Quarta';
+    return diaSemanaTexto;
     case 4:
-        diaSemanaTexto = 'Quinta';
-    break;
-        case 5:
-        diaSemanaTexto = 'Sexta';
-        break;
+    diaSemanaTexto = 'Quinta';
+    return diaSemanaTexto;
+    case 5:
+    diaSemanaTexto = 'Sexta';
+    return diaSemanaTexto;
     case 6:
-        diaSemanaTexto = 'Sabado';
-        break;
+    diaSemanaTexto = 'Sábado';
+    return diaSemanaTexto;
+    default:
+    diaSemanaTexto = '';
+    return diaSemanaTexto;
+    }
 }
+    const data = new Date('1987-04-21 00:00:00');
+    const diaSemana = data.getDay();
+    const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+    
+    console.log(diaSemana, diaSemanaTexto);
 
 // if (diaSemana === 0) {
 //     diaSemanaTexto = 'Domingo';
