727b97ea43f16210f684e9e7c7bcf469e3453200
Suzana Santos <suzanajb1@gmail.com>
2024-09-19 17:54:21 -0300

    atualizei o exercicio

--- exercicios/ex030/assets/js/main.js
+++ exercicios/ex030/assets/js/main.js
@@ -32,4 +32,65 @@ function getDiaSemanaTexto(diaSemana) {
     }
 }
 
-h1.innerHTML = getDiaSemanaTexto(data.getDay());
+function getNomeMes(numeroMes) {
+    let nomeMes;
+
+    switch (numeroMes) {
+    case 0:
+        nomeMes = 'janeiro';
+    return nomeMes;
+    case 1:
+        nomeMes = 'fevereiro';
+    return nomeMes;
+    case 2:
+        nomeMes = 'marco';
+    return nomeMes;
+    case 3:
+        nomeMes = 'abril';
+    return nomeMes;
+    case 4:
+        nomeMes = 'maio';
+    return nomeMes;
+    case 5:
+        nomeMes = 'junho';
+    return nomeMes;
+    case 6:
+        nomeMes = 'julho';
+    return nomeMes;
+    case 7:
+        nomeMes = 'agosto';
+    return nomeMes;
+    case 8:
+        nomeMes = 'setembro';
+    return nomeMes;
+    case 9:
+        nomeMes = 'outubro';
+    return nomeMes;
+    case 10:
+        nomeMes = 'novembro';
+    return nomeMes;
+    case 11:
+        nomeMes = 'dezembro';
+    return nomeMes;
+    }
+}
+
+function zeroAEquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function criaData(data){
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDiaSemanaTexto(diaSemana);
+    const nomeMes = getNomeMes(numeroMes);
+
+    return (
+        `${nomeDia}, ${data.getDate()} de ${nomeMes}` +
+        ` de ${data.getFullYear()} ` +
+        `${zeroAEquerda(data.getHours())}:${zeroAEquerda(data.getMinutes())}`
+    );
+}
+h1.innerHTML = criaData(data);
+
