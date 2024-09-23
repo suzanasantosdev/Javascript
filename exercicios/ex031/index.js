709285b557254ffb6f0757ff0d524f3b4f1909ec
Suzana Santos <suzanajb1@gmail.com>
2024-09-23 15:48:04 -0300

    finalizei o exercicio

--- exercicios/ex031/index.js
+++ exercicios/ex031/index.js
@@ -3,10 +3,17 @@ const verdadeira = true;
 //let tem escopo de bloco {... bloco }
 // var so tem escopo de funcao
 
-let nome = 'Suzana';
-var nome2 = 'Suzana';
+let nome = 'Suzana'; // Criando
+var nome2 = 'Suzana'; // Criando
 
 if (verdadeira) {
-    let nome = 'Suzana';
-    console.log(nome, nome2);
+    let nome = 'Suzana'; // Criando
+    var nome2 = 'David'; // Redeclarando
+
+    if (verdadeira) {
+        var nome2 = 'Joao';
+        let nome = 'Outra coisa';
+    }
 }
+
+console.log(nome, nome2);
