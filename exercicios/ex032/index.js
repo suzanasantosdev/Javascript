b951d700f425ac5d96c5cfe55f4800e201a763f3
Suzana Santos <suzanajb1@gmail.com>
2024-09-23 21:04:03 -0300

    terminei o exercicio 032 de arrays

--- exercicios/ex032/index.js
+++ exercicios/ex032/index.js
@@ -1,5 +1,15 @@
-let a = 'A'; // B
-let b = 'B'; // C
-let c = 'C'; // A
+// let a = 'A'; // B
+// let b = 'B'; // C
+// let c = 'C'; // A
 
-console.log(a,b,c);
+// [a,b,c] = [1,2,3];
+
+// console.log(a,b,c);
+
+
+              // 0 1 2 3 4 5 6 7 8
+const numeros = [1000,2000,3000,4000,5000,6000,7000,8000,9000];
+const [um, dois, tres, ...resto] = numeros;
+console.log(um, dois, tres);
+
+console.log(resto);
