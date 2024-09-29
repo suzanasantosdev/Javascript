9f6a81d5d3a26ba8965db5b65528614d65e679ae
Suzana Santos <suzanajb1@gmail.com>
2024-09-29 18:04:28 -0300

    atualizei o exercicio

---
+++ exercicios/ex034/index.js
@@ -0,0 +1,7 @@
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`Linha ${i}`);
+// }
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+const novasFrutas = frutas.map(el => el.toLowerCase());
+console.log(novasFrutas); // [ 'maçã', 'pêra', 'uva' ]
