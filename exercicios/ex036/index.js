b80ccc7c99eb53dda8ba8af3977e1d5628f63ed1
Suzana Santos <suzanajb1@gmail.com>
2024-10-10 20:33:16 -0300

    finalizei exercicio

--- exercicios/ex036/index.js
+++ exercicios/ex036/index.js
@@ -1,11 +1,18 @@
 // For in -> le os indices ou chaves do objeto
 
 //                0       1       2
-const frutas = ['Pera', 'Maça', 'Uva'];
+const pessoa = {
+    nome: 'Suzana',
+    Sobrenome: 'Santos',
+    Idade: 24
+};
 
-for (let indice in frutas) {
-    console.log(frutas[indice]);
+for (let chave in pessoa ) {
+    console.log(chave, pessoa[chave]);
 }
+// for (let indice in frutas) {
+//     console.log(frutas[indice]);
+// }
 
 // for (let i = 0; i < frutas.length; i ++) {
 //     console.log(frutas[i]);
