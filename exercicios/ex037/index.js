50ff55357aa20986a6443c9f1de768d9c1b8529e
Suzana Santos <suzanajb1@gmail.com>
2024-10-11 16:17:05 -0300

    finalizei o exercício

--- exercicios/ex037/index.js
+++ exercicios/ex037/index.js
@@ -1,1 +1,13 @@
 const nome = 'Suzana Santos';
+
+// for (let i = 0; i < nome.length; i++ ) {
+//     console.log(nome[i]);
+// }
+
+// for (let i in nome) {
+//     console.log(nome[i]);
+// }
+
+for (let valor of nome) {
+    console.log(valor);
+}
